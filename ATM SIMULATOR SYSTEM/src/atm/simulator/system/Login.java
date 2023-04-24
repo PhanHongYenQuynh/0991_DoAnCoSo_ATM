40d53da39f157e5ac8fb8ef07cc2e7160df97b9b
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-04-24 15:12:40 +0700

    Update loginSecurity by Quynh

--- ATM SIMULATOR SYSTEM/src/atm/simulator/system/Login.java
+++ ATM SIMULATOR SYSTEM/src/atm/simulator/system/Login.java
@@ -10,6 +10,7 @@ public class Login extends JFrame implements ActionListener {
     JButton login, signup, clear;
     JTextField cardTextField;
     JPasswordField pinTextField;
+  /*  int numAttempts = 0;*/
     Login(){
         setTitle("AUTOMATED TELLER MACHINE");
         setLayout(null);
@@ -83,32 +84,64 @@ public class Login extends JFrame implements ActionListener {
         setLocation(350, 200);
     }
 
-    public void actionPerformed(ActionEvent ae){
-        if(ae.getSource()==clear){
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == clear) {
             cardTextField.setText("");
             pinTextField.setText("");
-        }else if(ae.getSource() == login){
+        } else if (ae.getSource() == login) {
             Conn conn = new Conn();
             String cardnumber = cardTextField.getText();
             String pinnumber = pinTextField.getText();
-            String query = "select * from login where cardnumber = '"+cardnumber+"' and pin = '"+pinnumber+"'";
-            try{
-               ResultSet rs = conn.s.executeQuery(query);
-               if(rs.next()){
-                   setVisible(false);
-                   new Transactions(pinnumber).setVisible(true);
-               }else{
-                   JOptionPane.showMessageDialog(null, "Quý khách đã nhập sai mã pin!" +
-                           "\nVui lòng kiểm tra và nhập lại đúng mã pin.");
-               }
-            }catch(Exception e){
-                System.out.println(e);
+            String query = "SELECT * FROM login WHERE cardnumber = '"+cardnumber+"' AND pin = '"+pinnumber+"'";
+            try {
+                ResultSet rs = conn.s.executeQuery(query);
+                if (rs.next()) {
+                    Timestamp lockedUntil = rs.getTimestamp("locked_until");
+                    if (lockedUntil != null && lockedUntil.after(new Timestamp(System.currentTimeMillis()))) {
+                        JOptionPane.showMessageDialog(null, "Tài khoản của bạn đã bị khóa. Vui lòng liên hệ với số hotline để được hỗ trợ!");
+                        return;
+                    }
+                    setVisible(false);
+                    new Transactions(pinnumber).setVisible(true);
+                } else {
+                    // Lấy thông tin tài khoản từ database
+                    query = "SELECT * FROM login WHERE cardnumber = '" + cardnumber + "'";
+                    rs = conn.s.executeQuery(query);
+                    rs.next();
+                    int wrongAttempts = rs.getInt("wrong_attempts");
+                    Timestamp lockedUntil = rs.getTimestamp("locked_until");
+
+                    if (lockedUntil != null && lockedUntil.after(new Timestamp(System.currentTimeMillis()))) {
+                        JOptionPane.showMessageDialog(null, "Tài khoản của bạn đã bị khóa trong 10 phút.");
+                        return;
+                    } else if (wrongAttempts >= 5) {
+                        // Khoá tài khoản vĩnh viễn nếu nhập sai quá nhiều lần
+                        query = "UPDATE login SET locked_until = CURRENT_TIMESTAMP + INTERVAL 10 YEAR WHERE cardnumber = '" + cardnumber + "'";
+                        conn.s.executeUpdate(query);
+                        JOptionPane.showMessageDialog(null, "Tài khoản của bạn đã bị khóa vĩnh viễn.\nVui lòng liên hệ với số hotline để được hỗ trợ!");
+                        return;
+                    } else if (wrongAttempts >= 3) {
+                        // Khoá tài khoản trong 10 phút nếu nhập sai 3 lần
+                        lockedUntil = new Timestamp(System.currentTimeMillis() + 10 * 60 * 1000);
+                        query = "UPDATE login SET wrong_attempts = " + (wrongAttempts + 1) + ", locked_until = '" + lockedUntil + "' WHERE cardnumber = '" + cardnumber + "'";
+                        conn.s.executeUpdate(query);
+                        JOptionPane.showMessageDialog(null, "Quý khách đã nhập sai mã PIN!\nVui lòng kiểm tra và nhập lại đúng mã PIN.\nSố lần nhập sai: " + (wrongAttempts + 1));
+                    } else {
+                        // Cập nhật số lần nhập sai
+                        query = "UPDATE login SET wrong_attempts = " + (wrongAttempts + 1) + " WHERE cardnumber = '" + cardnumber + "'";
+                        conn.s.executeUpdate(query);
+                        JOptionPane.showMessageDialog(null, "Quý khách đã nhập sai mã PIN!\nVui lòng kiểm tra và nhập lại đúng mã PIN.\nSố lần nhập sai: " + (wrongAttempts + 1));
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        }else if(ae.getSource()==signup){
+        } else if (ae.getSource() == signup) {
             setVisible(false);
             new SignupOne().setVisible(true);
         }
     }
+
     public static void main(String args[]){
         new Login();
     }
