dda2bc28bf312caa92590b5ebb63d7f3c1f12808
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-04-26 20:33:14 +0700

    Update Twilio

--- ATM SIMULATOR SYSTEM/src/atm/simulator/system/SMS.java
+++ ATM SIMULATOR SYSTEM/src/atm/simulator/system/SMS.java
@@ -8,8 +8,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SMS {
-    public static final String ACCOUNT_SID = "ACec36b89d26705fbb11b8f10ce75fb408";
-    public static final String AUTH_TOKEN = "c5214da39d10f33affabf3934b23e0f0";
+    public static final String ACCOUNT_SID = "NULL";
+    public static final String AUTH_TOKEN = "NULL";
     public static final String TO_NUMBER = "+84986556949";
     public static final String FROM_NUMBER = "+16315576245";
 
